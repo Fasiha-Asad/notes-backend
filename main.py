37252af7f359b0171ac134f59bfeab18acb2ecf5
Fasiha Asad <fasihaasad513@gmail.com>
2026-07-27 18:28:12 -0700

    Created create endpoint

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-print("Working just fine")
-print("Working just fine")
-print("Working just fine")
+from fastapi import FastAPI 
+app=FastAPI() 
+notes=[] 
+
+@app.post("/create")
+def create_note(note: str): 
+    notes.append(note)
+    return {
+        "message":"Note created successfully",
+        "note":note
+    }
